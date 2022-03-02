70364bccf4648bf3273c216d949b27a1bc359cb1
Austin Wong <austinwong@lever-awong.mynetworksettings.com>
2022-03-02 11:47:10 -0500

    First commit Message

---
+++ jobsite.js
@@ -0,0 +1,34 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementsByClassName("header");
+
+// Get the offset position of the navbar
+var sticky = header[0].offsetTop;
+
+console.log(sticky);
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header[0].classList.add("sticky");
+  } else {
+    header[0].classList.remove("sticky");
+  }
+}
+
+$(document).ready(function(){
+    $.ajax({
+        url: "https://api.lever.co/v1/postings",
+        type: "GET",
+        username: 'dXAGwHW7qPMpftWB8sXy70Xygb4XZaALy4nEKjHrp3HfbZMZ',
+        password: '',
+        success: function(result){
+            console.log(result);
+        },
+        error:function(error){
+            console.log(`Error`)
+        }
+    })
+})
